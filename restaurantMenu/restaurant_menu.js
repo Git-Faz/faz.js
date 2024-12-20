d40558d12f2426eaac6dfa6a93394b6afb71a1dd
Git-Faz <fahad.blr@icloud.com>
2024-12-20 14:50:54 -0500

    successful

--- restaurantMenu/restaurant_menu.js
+++ restaurantMenu/restaurant_menu.js
@@ -2,5 +2,18 @@ const breakfastMenu=['Pancakes','Eggs','Oatmeal','Frittata'];
 const mainCourseMenu=['Steak','Pasta','Salmon'];
 const dessertMenu=['Cake','Ice Cream','Pudding','Fruit Salad'];
 
+//map() method
 const breakfastMenuItemsHTML = breakfastMenu.map((item,index) => `<p>Item ${index+1}: ${item}</p>`).join('');
 document.getElementById('breakfastMenuItems').innerHTML = breakfastMenuItemsHTML;
+
+//forEach method
+let mainCourseItem = '';
+mainCourseMenu.forEach((item,index) => {
+mainCourseItem += `<p>Item ${index+1}: ${item}</p>`});
+document.getElementById('maincourseMenuItems').innerHTML = mainCourseItem;
+
+//for loop
+let dessertItem='';
+for(let i=0;i<dessertMenu.length;i++){
+    dessertItem += `<p>Item ${i+1}: ${dessertMenu[i]}</p>`;}
+document.getElementById('dessertMenuItems').innerHTML = dessertItem;
